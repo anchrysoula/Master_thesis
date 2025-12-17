80bd8599e2245f9bdc36d36202a3476e6cd21583
anchrysoula <chrysoulanast@gmail.com>
2025-12-17 12:36:45 +0200

    definition of a class

---
+++ deep_gp/deep_kernel_class.py
@@ -0,0 +1,41 @@
+import torch
+import gpytorch
+
+# --- Feature extractor network ---
+class LargeFeatureExtractor(torch.nn.Sequential):
+    def __init__(self, data_dim):
+        super(LargeFeatureExtractor, self).__init__()
+        self.add_module('linear1', torch.nn.Linear(data_dim, 1000))
+        self.add_module('relu1', torch.nn.ReLU())
+        self.add_module('linear2', torch.nn.Linear(1000, 500))
+        self.add_module('relu2', torch.nn.ReLU())
+        self.add_module('linear3', torch.nn.Linear(500, 50))
+        self.add_module('relu3', torch.nn.ReLU())
+        self.add_module('linear4', torch.nn.Linear(50, 2))
+
+
+# --- GP regression model ---
+class GPRegressionModel(gpytorch.models.ExactGP):
+    def __init__(self, train_x, train_y, likelihood, data_dim):
+        super(GPRegressionModel, self).__init__(train_x, train_y, likelihood)
+        self.mean_module = gpytorch.means.ConstantMean()
+        self.covar_module = gpytorch.kernels.GridInterpolationKernel(
+            gpytorch.kernels.ScaleKernel(
+                gpytorch.kernels.RBFKernel(ard_num_dims=2)
+            ),
+            num_dims=2, grid_size=100
+        )
+        # Attach feature extractor
+        self.feature_extractor = LargeFeatureExtractor(data_dim)
+
+        # Scale NN features to [-1, 1]
+        self.scale_to_bounds = gpytorch.utils.grid.ScaleToBounds(-1., 1.)
+
+    def forward(self, x):
+        # Pass input through deep net
+        projected_x = self.feature_extractor(x)
+        projected_x = self.scale_to_bounds(projected_x)
+
+        mean_x = self.mean_module(projected_x)
+        covar_x = self.covar_module(projected_x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
