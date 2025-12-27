5959d9fcb3112823ed3825c89ff0cdbf55dd590c
anchrysoula <chrysoulanast@gmail.com>
2025-12-27 14:34:01 +0200

    upload some files

--- deep_gp/gptorch_example.py
+++ deep_gp/gptorch_example.py
@@ -13,3 +13,19 @@ class ExactGPModel(gpytorch.models.ExactGP):
         covar_x = self.covar_module(x)
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
+class RBFLinearGPModel(gpytorch.models.ExactGP):
+    def __init__(self, train_x, train_y, likelihood):
+        super().__init__(train_x, train_y, likelihood)
+        
+        self.mean_module = gpytorch.means.ConstantMean()
+        
+        # RBF + Linear kernel
+        self.covar_module = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.RBFKernel(ard_num_dims=train_x.shape[1]) +
+            gpytorch.kernels.LinearKernel()
+        )
+
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
